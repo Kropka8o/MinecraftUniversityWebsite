d60a137eed0d5b6ae8c7faca07bcd88eab6d95ef
Jasper Meijeren <jasper-05@outlook.com>
2025-10-28 15:48:48 +0100

    feature/parkour V1 done

--- pages/parkour.php
+++ pages/parkour.php
@@ -8,19 +8,41 @@
 </head>
 <body>
     <?php include '../header.php'; ?>
-    <header>
-        <h1>Parkour</h1>
-        <p>In this world you will find an awesome parkour course designed to challenge your skills and improve your gameplay. Parkours are a popular activity in Minecraft, allowing players to test their agility and precision. 
-            below you will find the information about this parkour.</p>
-    </header>
     <main>
-
-        <h2>The biomes</h2>
+    <div class="wrapper">
+        <div class="top-main-content">
+            <div class="top-main-text">
+                <h1>Parkour</h1>
+                <p>In this world you will find an awesome parkour course designed to challenge your skills and improve your gameplay. Parkours are a popular activity in Minecraft, allowing players to test their agility and precision. 
+                below you will find the information about this parkour.</p>
+            </div>
+        <img src="../img/parkour.jpg" alt="Parkour Course" id="parkour-image">
+        </div>
+        <div class="main-content">
+        <h1>Parkour Through Squares</h1>
         <ul>
-            <li><a href="course1.php">Beginner Parkour</a></li>
-            <li><a href="course2.php">Intermediate Parkour</a></li>
-            <li><a href="course3.php">Advanced Parkour</a></li>
+            <li><h2>Overview:</h2></li>
+            <p>Parkour Through Squares offers a raw and focused parkour experience across 10 minimalist, square-themed levels. The map emphasizes clean design over complex mechanics, making each jump feel intentional and satisfying.
+
+While the levels appear simple at first glance, they require tight movement, timing, and patience. With no checkpoints, you’ll need to stay focused one misstep, and it’s back to the start of the level.</p>
+            <li><h2>Features:</h2></li>
+            <ul>
+                <li>10  square-themed levels</li>
+                <li>Challenging jumps requiring precision and timing</li>
+                <li>No checkpoints for a pure parkour challenge</li>
+                <li>Clean, distraction-free design</li>
+                <li>optimalized for solo play,but also great with friends!</li>
+            </ul>
+            <li><h2>Notes</h2></li>
+            <ul>
+                <li>Best played in Minecraft Java Edition</li>
+                <li>Designed for Minecraft 1.21.5</li>
+                <li>Recommended to play in Adventure Mode</li>
+                <li>Practice patience and precision for success</li>
+            </ul>
         </ul>
+        </div>
+    </div>
     </main>
     <?php include '../footer.php'; ?>
 </body>
